7fedff2cd9a4dfcca4333064c6a85bb3d81dd515
Jason Tang <potatomaster5432@gmail.com>
2017-11-30 10:22:22 -0800

    Still using Variables

---
+++ ECSJavaProject/src/StillUsingVariables.java
@@ -0,0 +1,17 @@
+
+public class StillUsingVariables 
+{
+		public static void main(String[] args) 
+		{
+			int grad;
+			String yourName; 
+			
+			grad = 2005;
+			yourName = "Dank Memester";
+			
+			System.out.println( "My name is " + yourName + " and I'll graduate in " + grad + "." );
+			
+			
+		}
+
+}
