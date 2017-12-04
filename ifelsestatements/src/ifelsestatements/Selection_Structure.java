e53ebb94c66ef2aabb4d3755b549c50295169b34
Jason Tang <potatomaster5432@gmail.com>
2017-12-04 09:48:13 -0800

    some dank stuff

---
+++ ifelsestatements/src/ifelsestatements/Selection_Structure.java
@@ -0,0 +1,47 @@
+package ifelsestatements;
+
+public class Selection_Structure 
+{
+	public static void main(String[] args) 
+	{
+		int people = 30;
+		int cars = 40;
+		int buses = 15;
+		
+		if( cars > people )
+		{
+			System.out.println( "We should take the cars" );
+		}
+		else if( cars < people )
+		{
+			System.out.println( "We should not take the cars" );
+		}
+		else
+		{
+			System.out.println( "We can't decide" );
+		}
+		
+		if( buses > cars )
+		{
+			System.out.println( "That's too many buses" );
+		}
+		else if ( buses < cars )
+		{
+			System.out.println( "Maybe we should take the buses" );
+		}
+		else
+		{
+			System.out.println( "We can't decide" );
+		}
+		
+		if( people > buses )
+		{
+			System.out.println( "Let's take the buses" );
+		}
+		else
+		{
+			System.out.println( "Fine, let's stay home then" );
+		}
+	}
+
+}
